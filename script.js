1fd977730e21d224e2458b3749f32cc4c2fffcc2
NihaDas2012 <nihadas2012@gmail.com>
2025-03-05 21:14:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.querySelector("#submit").addEventListener("click", display)
+function display(){
+  if (!validateForm() == false) {
+      document.querySelector("#there").textContent = "Thank you for contacting us. We will get back to you as soon as possible"
+  }
+}
+
+function validateForm() {
+  var fname =   document.forms["ContactForm"]["fname"].value;
+  if (fname == "") {
+   alert("Please Enter your First Name");
+   return false;
+  }
+  var lname = document.forms["ContactForm"]["lname"].value;
+  if (lname == "") {
+    alert("Please Enter your Last Name");
+    return false;
+  }
+  var email = document.forms["ContactForm"]["email"].value;
+  if (email == "") {
+    alert("Please Enter your Email");
+    return false;
+  }
+  return ValidEmail()
+    var subject = document.forms["ContactForm"]["subject"].value;
+  if (subject == "") {
+    alert("Please Enter A Query");
+    return false;
+  }
+} 
+
+function ValidEmail{
+   var email = document.forms["ContactForm"]["email"].value;
+  if (email.includes(" ") || email.includes(",") || email.includes("-") || email.includes("_") || (!email.includes(".com") && !email.includes("@"))){
+        alert("Please Enter A Valid Email Address");
+    return false;
+  }
+}
